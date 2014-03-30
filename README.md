8584fac34e636689bee26693d373166c61d30129
Sarma Tangirala <vtangira@buffalo.edu>
2014-03-30 04:05:04 -0400

    Working example.

---
+++ README.md
@@ -0,0 +1,1 @@
+Scratch space to play with openGL.
--- first.cpp
+++ first.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
+#include <GL/glew.h>
 // Includes openGL and other constants.
 #include <GLFW/glfw3.h>
-#include <GL/glew.h>
 #include <glm/glm.hpp>
+
+void error_callback(int errno, const char *desc)
+{
+    std::cerr << "Code: " << errno << " Text: " << desc << std::endl;
+}
 
 int main(int argc, char **argv)
 {
+    glfwSetErrorCallback(error_callback);
     if (!glfwInit())
     {
         std::cerr << "GLFW not initialized." << std::endl;
@@ -14,9 +20,10 @@ int main(int argc, char **argv)
     }
 
     glfwWindowHint(GLFW_SAMPLES, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Working with 3.1 for now.
+    /*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
 
     GLFWwindow *window;
     window = glfwCreateWindow(1024, 768, "First", NULL, NULL);
@@ -34,6 +41,17 @@ int main(int argc, char **argv)
         std::cerr << "Failed to initialize glew" << std::endl;
         return -1;
     }
+
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS
+           && glfwWindowShouldClose(window) == 0)
+    {
+        // Simply swap buffers for now.
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 
     return 0;
 }
